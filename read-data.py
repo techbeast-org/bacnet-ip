8782c164d9e426dd27b2556c9d9a77e1424a1237
Venkat Subramanian <apvenkat@Venkats-MacBook-Air.local>
2022-05-23 18:04:40 +0800

    added comments

--- read-data.py
+++ read-data.py
@@ -1,14 +1,17 @@
 import BAC0
 import json
 import time
+# create a bacnet connection to connect to the devices, use your local machine IP address and the subnet below and also specify the port number if needed
 bacnet = BAC0.lite(ip="192.168.18.6/24" ,port=47809)  
 data = {}
 
 
 def main():
     while True:
+        # Reading 3 Objects from the Bacnet Simulator
         for x in range(0, 3):
             id = str(x)
+        # Connect to the BACnet simulator using the IP address and port number
             value = bacnet.read(
                 "192.168.18.10:53522 analogInput "+id+" presentValue")
             value = str(value)
@@ -17,6 +20,6 @@ def main():
         print(json.dumps(data, indent=2))
         time.sleep(5)
 
-
+#RUN main
 if __name__ == '__main__':
     main()
